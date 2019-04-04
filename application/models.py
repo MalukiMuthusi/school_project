fe4835d1800e15883aa1e2907d7ec57604c99f74
MalukiMuthusi <malukimuthusi@yahoo.com>
2019-04-04 20:39:07 +0300

    did list of all schools page
    added absolute url for each school

--- application/models.py
+++ application/models.py
@@ -1,6 +1,5 @@
-import uuid
-from random import randint
 import datetime
+from django.urls import reverse
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -11,79 +10,136 @@ from django.core.exceptions import ValidationError
 
 class Student(models.Model):
     """Model representing student."""
+
     first_name = models.CharField(
-        'First name', max_length=200, help_text='Enter first name', db_column='first_name')
+        "First name",
+        max_length=200,
+        help_text="Enter first name",
+        db_column="first_name",
+    )
     last_name = models.CharField(
-        'Last name', max_length=200, help_text='Enter last name', db_column='last_name')
+        "Last name", max_length=200, help_text="Enter last name", db_column="last_name"
+    )
     admission_number = models.PositiveSmallIntegerField(
-        'Admission number', unique=True, help_text='Enter admission number', db_column='admission_number')
+        "Admission number",
+        unique=True,
+        help_text="Enter admission number",
+        db_column="admission_number",
+    )
     Date_of_Birth = models.DateField(
-        'Date of birth', help_text='Enter date of birth', default='1998-02-02', db_column='date_of_birth')
+        "Date of birth",
+        help_text="Enter date of birth",
+        default="1998-02-02",
+        db_column="date_of_birth",
+    )
     current_class = models.IntegerField(
-        'current class', help_text="Enter the student's current class", db_column='current_class')
-    school = models.ForeignKey(
-        'School', on_delete=models.CASCADE, db_column='school')
+        "current class",
+        help_text="Enter the student's current class",
+        db_column="current_class",
+    )
+    school = models.ForeignKey("School", on_delete=models.CASCADE, db_column="school")
     home_town = models.CharField(
-        'Home town', max_length=200, help_text='Enter the location "County/Constituence/Town"', blank=True, db_column='home_town')
+        "Home town",
+        max_length=200,
+        help_text='Enter the location "County/Constituence/Town"',
+        blank=True,
+        db_column="home_town",
+    )
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
     class Meta:
-        ordering = ['admission_number']
-        db_table = 'students'
-    
+        ordering = ["admission_number"]
+        db_table = "students"
+
     def clean(self):
-        #if  in primary school then  classes should 1-8 
-        if self.school.school_category == 'PRI_SCH':
-            self.current_class == 1 or self.current_class == 2 or self.current_class ==3 or self.current_class  ==4 or self.current_class == 5 or self.current_class == 6 or self.current_class ==7 or self.current_class ==8
-        else :
-            raise ValidationError(_('Class 1-8'))
+        # if  in primary school then  classes should 1-8
+        if self.school.school_category == "PRI_SCH":
+            self.current_class == 1 or self.current_class == 2 or self.current_class == 3 or self.current_class == 4 or self.current_class == 5 or self.current_class == 6 or self.current_class == 7 or self.current_class == 8
+        else:
+            raise ValidationError(_("Class 1-8"))
 
         if self.Date_of_Birth >= datetime.date.today():
-            raise ValidationError(_('Invalid Date of Birth. Date too small'))
+            raise ValidationError(_("Invalid Date of Birth. Date too small"))
 
-        if self.Date_of_Birth <= datetime.date(1997,1,1):
-            raise ValidationError(_('Invalid date of birth. Date too large.'))
+        if self.Date_of_Birth <= datetime.date(1997, 1, 1):
+            raise ValidationError(_("Invalid date of birth. Date too large."))
+
 
 class Parent(models.Model):
     first_name = models.CharField(
-        'First name', max_length=200, help_text='Enter first name', db_column='first_name')
+        "First name",
+        max_length=200,
+        help_text="Enter first name",
+        db_column="first_name",
+    )
     last_name = models.CharField(
-        'Last name', max_length=200, help_text='Enter last name', db_column='last_name')
+        "Last name", max_length=200, help_text="Enter last name", db_column="last_name"
+    )
     email_address = models.EmailField(
-        max_length=254, help_text='Enter email address.', db_column='email_address')
+        max_length=254, help_text="Enter email address.", db_column="email_address"
+    )
     student = models.ManyToManyField(
-        'Student', help_text="students names", db_column='student')
+        "Student", help_text="students names", db_column="student"
+    )
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
     class Meta:
-        ordering = ['last_name', 'first_name']
-        db_table = 'parents'
+        ordering = ["last_name", "first_name"]
+        db_table = "parents"
 
 
 class School(models.Model):
     school_name = models.CharField(
-        'School name', max_length=200, help_text='Enter last name', db_column='school_name')
+        "School name",
+        max_length=200,
+        help_text="Enter last name",
+        db_column="school_name",
+    )
     number_of_teachers = models.IntegerField(
-        'Number of Teachers in School', help_text='Enter the number of teaching stuff', default=40, db_column='number_of_teachers')
+        "Number of Teachers in School",
+        help_text="Enter the number of teaching stuff",
+        default=40,
+        db_column="number_of_teachers",
+    )
     number_of_students = models.IntegerField(
-        'Total students', help_text='Enter the total number of students', default=200, db_column='number_of_students')
+        "Total students",
+        help_text="Enter the total number of students",
+        default=200,
+        db_column="number_of_students",
+    )
     school_location = models.CharField(
-        'School Location', max_length=200, help_text='Enter "County/Constituence/Town"', blank=True, db_column='school_location')
+        "School Location",
+        max_length=200,
+        help_text='Enter "County/Constituence/Town"',
+        blank=True,
+        db_column="school_location",
+    )
     year_established = models.DateField(
-        'Date Established', help_text='Enter the date in which school was established', default='01/01/2000', db_column='year_established')
-    SCHOOL_CATEGORY = (('PRI_SCH', 'Primary School'),
-                       ('SEC_SCH', 'Secondary School'))
+        "Date Established",
+        help_text="Enter the date in which school was established",
+        default="01/01/2000",
+        db_column="year_established",
+    )
+    SCHOOL_CATEGORY = (("PRI_SCH", "Primary School"), ("SEC_SCH", "Secondary School"))
     school_category = models.CharField(
-        'School level', help_text='Select school level', choices=SCHOOL_CATEGORY, default='SEC_SCH', max_length=20, db_column='school_category')
+        "School level",
+        help_text="Select school level",
+        choices=SCHOOL_CATEGORY,
+        default="SEC_SCH",
+        max_length=20,
+        db_column="school_category",
+    )
+
+    def get_absolute_url(self):
+        return reverse("school", kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.school_name
 
     class Meta:
-        db_table = 'schools'
-        ordering = ['school_category',
-                    'number_of_students', 'number_of_teachers']
+        db_table = "schools"
+        ordering = ["school_category", "number_of_students", "number_of_teachers"]
--- application/views.py
+++ application/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views import generic
+from django.views.generic import TemplateView
 
 from application.models import Parent, School, Student
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def school(request, pk):
+    school_name = School.objects.get(pk=pk)
+    context = {
+        "name": school_name,
+        "year": School.year_established,
+        "sch_level": School.school_category,
+    }
+    return render(request, "school.html", context=context)
+
+
+def all_schools(request):
+    all_entries = School.objects.all()
+    pri_sch = School.objects.filter(school_category="PRI_SCH")
+    sec_sch = School.objects.filter(school_category="SEC_SCH")
+    context = {
+        "all_entries": all_entries,
+        "primo": pri_sch,
+        "sec": sec_sch
+        }
+    page = request.GET.get('page', 1)
+    paginator = Paginator(all_entries, 5)
+    return render(request, "all_schools.html", context=context)
+
