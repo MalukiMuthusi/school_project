96271150b34227032ec2326b396294cacae9151e
MalukiMuthusi <malukimuthusi@yahoo.com>
2019-04-06 23:48:55 +0300

    added Tabular  inline for related items

--- application/views.py
+++ application/views.py
@@ -88,7 +88,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {username}!")
-            return redirect("index")
+            return redirect("index", permanent=True)
     else:
         form = SchoolRegisterForm()
     return render(request, "school_reg.html", {"form": form})
